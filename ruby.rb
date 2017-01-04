47ac33479502ef73c8ceed401476397494e27d7a
Aleksander Katasonov <kataslon@yandex.ru>
2017-01-04 21:25:26 +0200

    added more exercises

--- ruby.rb
+++ ruby.rb
@@ -5,8 +5,11 @@
 
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-even = array.map.with_index { |el, ind| el if ind%2 == 0 }.compact
-odd = array.map.with_index { |el, ind| el if ind%2 != 0 }.compact
+# even = array.map.with_index { |el, ind| el if ind%2 == 0 }.compact
+# odd = array.map.with_index { |el, ind| el if ind%2 != 0 }.compact
+
+even = array.select.with_index { |el, ind| el if ind.even? }
+odd = array.select.with_index { |el, ind| el if ind.odd? }
 
 result = even + odd #=> [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
 
@@ -143,7 +146,7 @@ result = array.select { |i| i == array.max }.size
 # 44 Дан целочисленный массив. Найти максимальный нечетный элемент.
 
 array = [2, 4, 5, 7, 9, 5, 6, 9, 2, 7]
-result = array.select { |i| i%2 != 0 }.max
+result = array.select { |i| i.odd? }.max
 
 # 48 Дан целочисленный массив и интервал a..b. Найти максимальный из элементов в этом интервале.
 
@@ -211,8 +214,7 @@ result = array.map { |i| i*i }.inject(0) { |sum, n| sum + n } / array.size
 # расположенных в этом диапазоне (исключая сами эти числа), в порядке их возрастания, а также размер этого массива.
 
 range = a+1...b
-result = range.each_with_object([]) { |el, a| a << el }.sort
-size = result.size
+result = range.each_with_object([]) { |el, a| a << el }.sort.size
 
 
 # 84 Дано натуральное число N. Найти результат следующего произведения 1*2*…*N.
@@ -223,7 +225,7 @@ size = result.size
 # 88 Дан целочисленный массив. Найти количество четных элементов.
 
 array = [1, 3, 5, 4, 5, 4, 6, 7, 5, 4, 6, 5, 2, 7]
-result = array.map { |el| el if el%2 == 0 }.compact.size
+result = array.map { |el| el if el.even? }.compact.size
 
 
 # 92 Дан целочисленный массив и число К. Если существует элемент, больший К, то вывести true; в противном случае вывести false.
@@ -254,3 +256,123 @@ array = [1, 3, 5, 4, 5, 4, 6, 7, 5, 4, 6, 5, 2, 7]
 result = array.map { |el| el ** K }
 
  (4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108)
+
+
+
+
+
+
+# Сортировка массива: чет/нечет, обратный порядок, массив строк упорядочить по длине слов (группировка по длине слов)
+
+# чет/нечет
+array = [1, 3, 5, 4, 5, 4, 6, 7, 5, 4, 6, 5, 2, 7]
+array.sort_by { |i| [i%2, i] } # Женя показал:)
+
+even = array.select { |el| el if el.even? }.sort
+odd = array.select { |el| el if el.odd? }.sort
+result = even + odd
+
+# обратный порядок
+
+array.each_with_object([]) { |el, a| a.unshift(el) }
+
+# массив строк упорядочить по длине слов (группировка по длине слов)
+
+array = ['indexing starts at 0,', 'as in C', 'or', 'Java. A negative', 'index is assumed to be relative']
+array.sort { |x, y| x.length <=> y.length }.group_by{|i| i.length}
+
+
+# Поиск в массиве: локальные максимумы
+
+array = [1, 3, 5, 4, 5, 4, 6, 7, 5, 4, 6, 5, 2, 7]
+array.map.with_index { |el, ind| el if el > (array[ind-1] ? array[ind-1] : 0) && el > (array[ind+1] ? array[ind+1] : 0)}.compact
+
+# написать map, select, detect, count, all?, any? через reduce (своя реализация)
+module ArrayExtantion
+  #map
+    def self.reduce_map(array)
+      array.reduce([]){ |a, el| block_given? ? a << yield(el) : "#<Enumerator: #{array}:reduce_map>" }
+    end
+
+  # select
+    def self.reduce_select(array)
+      array.reduce([]){ |a, el| block_given? ? (yield(el) ? a << el : a) : "#<Enumerator: #{array}:reduce_select>" }
+    end
+
+  # count
+    def self.reduce_count(array, n=nil)
+      if n
+        reduce_select(array) { |el| el == n }.reduce(0) { |a| a + 1 }
+      elsif block_given?
+        array.reduce(0) { |a, el| yield(el) ? a + 1 : a }
+      else
+        array.reduce(0) { |a| a + 1 }
+      end
+    end
+
+  # all?
+    def self.reduce_all?(array)
+      if block_given?
+        !array.reduce([]) { |a, el| a << false if yield(el) }.include? false
+      else
+        !array.reduce([]) { |a, el| a << false if el != nil && el != false }.include? false
+      end
+    end
+
+  # any?
+    def self.reduce_any?(array)
+      if block_given?
+        array.reduce(false) { |a, el| return true if yield(el); a }
+      else
+        array.reduce(false) { |a, el| return true if el != nil && el != false; a }
+      end
+    end
+
+  # flatten
+    def self.flatten(array, res=[])
+      array.each do |item|
+        if item.is_a?(Array)
+          flatten(item, res)
+        else
+          res << item
+        end
+      end
+      res
+    end
+end
+
+ArrayExtantion::reduce_map(array)
+ArrayExtantion::rreduce_select(array)
+ArrayExtantion::reduce_count(array)
+ArrayExtantion::reduce_all?(array)
+ArrayExtantion::reduce_any?(array)
+
+array = [[[1, 2, 3, 4], 5, 5, 7], 7, 4, [3, 5, [4, 5, 6]]]
+ArrayExtantion::flatten(array)
+=> [1, 2, 3, 4, 5, 5, 7, 7, 4, 3, 5, 4, 5, 6]
+
+# hash merge
+
+  def merge(h1, h2)
+    res = {}
+    h1.each do |k, val|
+      res[k] = h2[k] ? val + h2[k] : val
+    end
+    keys = h2.keys - res.keys
+    keys.each do |k|
+      res[k] = h2[k]
+    end
+    res
+  end
+
+h1 = { "a" => 100, "b" => 200 }
+h2 = { "b" => 254, "c" => 300 }
+=> {"a"=>100, "b"=>454, "c"=>300}
+
+
+# группировка(дан массив имен-фамилий, написать алгоритм поиска однофамильцев)
+
+array = ["Alexander Dmitrenko", "Alexander Katasonov", "Anastasia Sakhno", "Nickolay Katasonov", "Andrey Katasonov"]
+
+array.map! { |item| item.split(' ').reverse }
+array.each_with_object(Hash.new) { |item, a| a[item[0]] = array.select { |i| i[0] == item[0] } }
